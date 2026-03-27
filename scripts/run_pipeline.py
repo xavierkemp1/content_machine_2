b2e8c171c16a77988e2a1069b87906e3946a3fba
xavierkemp1 <xavierkemp04@gmail.com>
2026-03-27 14:45:47 +0000

    Update run_pipeline.py

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,7 +1,8 @@
 """Entrypoint script for pipeline skeleton."""
+from dotenv import load_dotenv
+load_dotenv()  # loads .env from current working directory by default
 
 from content_machine import run_pipeline
-
 
 if __name__ == "__main__":
     run_pipeline()
